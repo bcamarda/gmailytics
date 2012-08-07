1f72a55b413dea1424b01234ae82a3d0afc1b1e5
Brett Camarda <bcamarda@gmail.com>
2012-08-06 19:28:12 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EmailProfiler::Application.config.secret_token = '2718cb39716dd490005a698874ed3063486a14aeb7c096511be34c5e9af5efd4e25d959f7bae32c3aa960f6af72e5cdb8e1c29b149d7993b4932e3abfef2f361'
